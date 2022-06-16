ac8e0cbec126a893ae4590dea18b6628cbb3aa37
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-16 17:39:51 +0700

    Implementing higher score

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 
 let score=20;
 let secretNumber = Math.trunc(Math.random()*20)+1;
+let highscore=0
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
     
@@ -21,7 +22,8 @@ document.querySelector('.check').addEventListener('click',function(){
         
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.number').style.width= '30rem';
-        document.querySelector('.highscore').textContent=score;
+        highscore=score>highscore?score:highscore;
+        document.querySelector('.highscore').textContent=highscore;
 
     }else if(guess> secretNumber){
         if(score>0){
