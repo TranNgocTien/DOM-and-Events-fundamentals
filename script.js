b760331ddeccbb5fbbf9be77dfe5f623dc3b2692
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-16 17:33:50 +0700

    Coding chalenge #1

--- script.js
+++ script.js
@@ -7,21 +7,21 @@
 // document.querySelector('.guess').value=23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
 let score=20;
-document.querySelector('.number').textContent = number;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
-
+    
     console.log(guess, typeof guess);
     if(!guess){
         document.querySelector('.message').textContent= '⛔️ No number!';
-    }else if( guess === number){
+    }else if( guess === secretNumber){
         document.querySelector('.message').textContent= '🎉 Correct Number!';
         document.querySelector('body').style.backgroundColor='#60b347';
-
-        document.querySelector('.number').style.width= '30rem';
         
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.number').style.width= '30rem';
+        document.querySelector('.highscore').textContent=score;
 
     }else if(guess> secretNumber){
         if(score>0){
@@ -39,3 +39,15 @@ document.querySelector('.check').addEventListener('click',function(){
     }
 
 });
+
+document.querySelector('.again').addEventListener('click',function(){
+    score=20;
+    document.querySelector('.score').textContent=score;
+    secretNumber=Math.trunc(Math.random()*20)+1;
+    document.querySelector('.message').textContent='Start guessing...';
+    document.querySelector('.number').textContent='?';
+    document.querySelector('.guess').value=0;
+
+    document.querySelector('body').style.backgroundColor='#222';
+    document.querySelector('.number').style.width='15rem';
+})
